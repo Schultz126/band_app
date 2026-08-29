e6a72af8c37a8e46b0841e05c01f58fa88990bec
Schultz126 <rafael.drzevieski@gmail.com>
2026-08-29 08:02:39 -0300

    Initial commit

---
+++ src/dummie_data/songs.ts
@@ -0,0 +1,12 @@
+import Song from "./Song";
+
+export const SONGS = [
+  new Song("Song 1", "E", new Date("2026-08-01"), "Tocar", true),
+  new Song("Song 2", "D", new Date("2026-06-05"), "Ensaiar", true),
+  new Song("Song 3", "E", new Date("2026-07-29"), "Tocar", false),
+  new Song("Song 4", "E", new Date("2026-03-29"), "Tocar", true),
+  new Song("Song 5", "E", new Date("2026-08-12"), "Tirar", false),
+  new Song("Song 6", "D", new Date("2026-04-18"), "Tirar", false),
+  new Song("Song 7", "C", new Date("2026-08-29"), "Ensaiar", true),
+  new Song("Song 8", "E", new Date("2026-08-29"), "Tocar", true),
+];
